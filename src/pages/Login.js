2edb3b7ed4b2f8b693aad0fbf7658f7bb1ff6e6c
Abdrr97 <abdrr97@gmail.com>
2021-03-15 15:56:59 +0100

    setting up tailwindcss

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,12 +13,20 @@ const Login = () => {
   const handleLogin = () => {}
 
   useEffect(() => {
-    document.title = 'Login | FireGrame'
+    document.title = 'Login | Insatgram'
     return () => {}
   }, [])
   return (
-    <div>
-      <h1>LOGIN</h1>
+    <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
+      <div className='flex w-3/5'>
+        <img
+          src='/images/iphone-with-profile.jpg'
+          alt='instagram app login pic'
+        />
+      </div>
+      <div className='flex flex-col w-2/5'>
+        <p>forms!</p>
+      </div>
     </div>
   )
 }
